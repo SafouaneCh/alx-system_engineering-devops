e6cb3a909f84b92b444fb67a0058cbdce1ba5a2d
SafouaneCh <safouanecharki3@gmail.com>
2024-08-15 01:15:20 -0700

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,12 +16,12 @@ def top_ten(subreddit):
         "https://www.reddit.com/r/{}/hot.json".format(subreddit),
         headers={"User-Agent": "Custom"},
         params={"limit": 10},
-        )
+    )
 
     if req.status_code == 200:
         for get_data in req.json().get("data").get("children"):
             dat = get_data.get("data")
             title = dat.get("title")
             print(title)
-        else:
-            print(None)
+    else:
+        print(None)
